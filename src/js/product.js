75dd5cd9214b22edd1b5993d309e0313628f2293
Kevin Morales <kevin.morales05@epn.edu.ec>
2023-05-11 08:22:14 -0500

    Ponder: Team Activity 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,21 +1,10 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
+import  productDetails  from "./productDetails.mjs";
 
-function addProductToCart(product) {
-  let cartItems = getLocalStorage("so-cart");
-  if (!Array.isArray(cartItems)) {
-    cartItems = [];
-  }
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const productID = getParam('product');
+console.log(findProductById(productID));
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+productDetails(productID);
+
+
---
+++ src/js/productDetails.mjs
@@ -0,0 +1,44 @@
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { findProductById } from "./productData.mjs";
+
+let product = {};
+
+export default async function productDetails(productId) {
+  // get the details for the current product. findProductById will return a promise! use await or .then() to process it
+  product = await findProductById(productId);
+  // once we have the product details we can render out the HTML
+  renderProductDetails();
+  // once the HTML is rendered we can add a listener to Add to Cart button
+  document.getElementById("addToCart").addEventListener("click", addToCartHandler);
+}
+
+
+
+function addProductToCart(productToAdd) {
+  let cartItems = getLocalStorage("so-cart");
+  if (!Array.isArray(cartItems)) {
+    cartItems = [];
+  }
+  cartItems.push(productToAdd);
+  setLocalStorage("so-cart", cartItems);
+}
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const productToAdd = await findProductById(e.target.dataset.id);
+  addProductToCart(productToAdd);
+}
+
+
+function renderProductDetails() {
+  document.querySelector("#productName").innerText = product.Brand.Name;
+  document.querySelector("#productNameWithoutBrand").innerText =
+    product.NameWithoutBrand;
+  document.querySelector("#productImage").src = product.Image;
+  document.querySelector("#productImage").alt = product.Name;
+  document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
+  document.querySelector("#productColorName").innerText =
+    product.Colors[0].ColorName;
+  document.querySelector("#productDescriptionHtmlSimple").innerHTML =
+    product.DescriptionHtmlSimple;
+  document.querySelector("#addToCart").dataset.id = product.Id;
+}
