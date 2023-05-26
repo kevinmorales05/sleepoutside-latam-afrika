3d1d25ba29c09ac78a436bb230b434cda5d8c3a6
anneozolin <anelisa.ozolin@gmail.com>
2023-05-25 22:46:21 -0300

    correctiong the class ProductData

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,11 +1,11 @@
-import { findProductById } from "./productData.mjs";
+import ProductData from "./productData.mjs";
 import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 
 let product = {};
 
 export default async function productDetails(productId) {
   // get the details for the current product. findProductById will return a promise! use await or .then() to process it
-  product = await findProductById(productId);
+  product = await ProductData.findProductById(productId);
   // once we have the product details we can render out the HTML
   renderProductDetails();
   // once the HTML is rendered we can add a listener to Add to Cart button
