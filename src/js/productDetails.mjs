1724f9825003226d245fce96f711a0fe957b00d8
anneozolin <anelisa.ozolin@gmail.com>
2023-05-25 22:36:03 -0300

    changed product details

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,5 +1,5 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 
 let product = {};
 
@@ -9,30 +9,23 @@ export default async function productDetails(productId) {
   // once we have the product details we can render out the HTML
   renderProductDetails();
   // once the HTML is rendered we can add a listener to Add to Cart button
-  document.getElementById("addToCart").addEventListener("click", addToCartHandler);
+  document.getElementById("addToCart").addEventListener("click", addToCart);
 }
-
-
-function addProductToCart(productToAdd) {
-  let cartItems = getLocalStorage("so-cart");
-  if (!Array.isArray(cartItems)) {
-    cartItems = [];
+function addToCart() {
+  let cartContents = getLocalStorage("so-cart");
+  //check to see if there was anything there
+  if (!cartContents) {
+    cartContents = [];
   }
-  cartItems.push(productToAdd);
-  setLocalStorage("so-cart", cartItems);
+  // then add the current product to the list
+  cartContents.push(product);
+  setLocalStorage("so-cart", cartContents);
 }
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const productToAdd = await findProductById(e.target.dataset.id);
-  addProductToCart(productToAdd);
-}
-
-
 function renderProductDetails() {
   document.querySelector("#productName").innerText = product.Brand.Name;
   document.querySelector("#productNameWithoutBrand").innerText =
     product.NameWithoutBrand;
-  document.querySelector("#productImage").src = product.Image;
+  document.querySelector("#productImage").src = product.Images.PrimaryLarge;
   document.querySelector("#productImage").alt = product.Name;
   document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
   document.querySelector("#productColorName").innerText =
